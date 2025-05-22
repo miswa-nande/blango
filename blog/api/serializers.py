27192147edc385a8bb769ff645a6cc15f122e160
Charles Ivan De Vera <charlesivan.ornales.devera@gmail.com>
2025-05-22 02:43:20 +0000

    Remove .pyc files and add .gitignore

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -1,11 +1,17 @@
 from rest_framework import serializers
+<<<<<<< HEAD
 from blog.models import Post, Tag
 from django.contrib.auth.models import User
+=======
+from blango_auth.models import User
+from blog.models import Post, Tag, Comment
+>>>>>>> Finish nested relationships
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+<<<<<<< HEAD
         fields = ["first_name", "last_name", "email"]
 
 class PostSerializer(serializers.ModelSerializer):
@@ -19,9 +25,59 @@ class PostSerializer(serializers.ModelSerializer):
         queryset=User.objects.all(),
         view_name="api_user_detail",
         lookup_field="email"
+=======
+        fields = ["id", "email", "first_name", "last_name"]
+
+
+class TagField(serializers.SlugRelatedField):
+    def to_internal_value(self, data):
+        try:
+            return self.get_queryset().get_or_create(value=data.lower())[0]
+        except (TypeError, ValueError):
+            self.fail(f"Tag value {data} is invalid")
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+    creator = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ["id", "creator", "content", "modified_at", "created_at"]
+        read_only_fields = ["modified_at", "created_at"]
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+    tags = TagField(
+        queryset=Tag.objects.all(), slug_field="value", many=True
+>>>>>>> Finish nested relationships
     )
 
     class Meta:
         model = Post
+<<<<<<< HEAD
         fields = "__all__"
         read_only_fields = ["modified_at", "created_at"]
+=======
+        fields = ["id", "author", "title", "slug", "summary", "content", "tags", "created_at", "modified_at"]
+
+
+class PostDetailSerializer(PostSerializer):
+    comments = CommentSerializer(many=True)
+
+    def update(self, instance, validated_data):
+        comments = validated_data.pop("comments")
+        instance = super(PostDetailSerializer, self).update(instance, validated_data)
+
+        for comment_data in comments:
+            if comment_data.get("id"):
+                continue
+            comment = Comment(**comment_data)
+            comment.creator = self.context["request"].user
+            comment.content_object = instance
+            comment.save()
+
+        return instance
+
+>>>>>>> Finish nested relationships
--- blog/api/views.py
+++ blog/api/views.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from rest_framework import generics, viewsets
 from blog.models import Post
 from blog.api.serializers import PostSerializer, UserSerializer
@@ -33,11 +34,22 @@ class PostViewSet(viewsets.ModelViewSet):
 
 
 class PostList(generics.ListCreateAPIView):
+=======
+from rest_framework import generics
+from blog.api.serializers import PostSerializer, UserSerializer, PostDetailSerializer
+from blog.models import Post
+from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
+
+
+class PostList(generics.ListCreateAPIView):
+    permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
+>>>>>>> Finish nested relationships
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+<<<<<<< HEAD
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
@@ -46,3 +58,9 @@ class UserDetail(generics.RetrieveAPIView):
     lookup_field = "email"
     queryset = User.objects.all()
     serializer_class = UserSerializer
+=======
+    permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
+    queryset = Post.objects.all()
+    serializer_class = PostDetailSerializer
+
+>>>>>>> Finish nested relationships
